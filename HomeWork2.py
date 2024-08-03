c136f624b417504f978f5e87de13262a38c09020
AlinaHan1 <alinakoshenko@gmail.com>
2024-08-03 23:02:36 +0300

    Add files via upload

---
+++ HomeWork2.py
@@ -0,0 +1,6 @@
+quantity_hw=12
+quantity_t=1.5
+course_name=('Python')
+print(quantity_t / quantity_hw)
+time=0.125
+print("Курс:",course_name,",всегозадач:",quantity_hw,",затрачено часов:",quantity_t,",среднее время выполнения:",time)
